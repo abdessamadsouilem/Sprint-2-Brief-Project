6c219559b15bd4442793e9699a12916a6a2052ea
abdessamad <abdessamadsouilem1@gmail.com>
2021-03-27 11:59:37 +0100

    go

--- Parti-code/HTML-CSS-SASS-JAVASCRIPT/update.php
+++ Parti-code/HTML-CSS-SASS-JAVASCRIPT/update.php
@@ -14,6 +14,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/main.css">
+    <link rel="shortcut icon" href="image/logo/Untitled-2.png" />
     <title>Elbahja Food</title>
 </head>
 <body>
